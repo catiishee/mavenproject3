7686233f95ec5c6a2aa30faf37046aa38ced4eda
Kate <164188026+catiishee@users.noreply.github.com>
2024-05-15 18:16:44 +0300

    3LabaIsSuper

--- src/main/java/mavenproject3/GUI.java
+++ src/main/java/mavenproject3/GUI.java
@@ -9,6 +9,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
@@ -38,8 +41,15 @@ public class GUI extends JFrame {
         importButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                File currentDirectory = null;
+                try {
+                    currentDirectory = new File(getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath()).getParentFile();
+                } catch (URISyntaxException ex) {
+                    Logger.getLogger(GUI.class.getName()).log(Level.SEVERE, null, ex);
+                }
                 JFileChooser fileChooser = new JFileChooser();
                 FileNameExtensionFilter filter = new FileNameExtensionFilter("JSON, XML & YAML Files", "json", "xml", "yaml");
+                fileChooser.setCurrentDirectory(currentDirectory);
                 fileChooser.setFileFilter(filter);
                 int returnVal = fileChooser.showOpenDialog(GUI.this);
                 if (returnVal == JFileChooser.APPROVE_OPTION) {
